81577ca4cd65d9180baddcd6d12522764d1c135c
Gyana prkakash Khandual <gyanaprakashkhnadual@gmail.com>
2025-09-03 13:01:36 +0530

    Metronque page initial commit

--- src/app/page.js
+++ src/app/page.js
@@ -1,3 +1,4 @@
+import MetroniqueHomePage from "./pages/web/Metronique";
 import RabbitHomePage from "./pages/web/Rabbit";
 import VeloriaHomepage from "./pages/web/Veloria";
 
@@ -5,7 +6,7 @@ import VeloriaHomepage from "./pages/web/Veloria";
 export default function Home() {
   return(
     <div>
-     <VeloriaHomepage/>
+     <MetroniqueHomePage/>
     </div>
   )
 }
